20dbc8a9b2f2b7c0db3e03d62e76b475c6c31962
Bosu <kotchapoom.ka04@gmail.com>
2023-07-15 04:47:10 +0700

    edit media screen

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,15 +4,17 @@ import { useCart } from "../context/CartContext";
 export default function Cart() {
   // eslint-disable-next-line
   const { products, total, formatMoney } = useCart();
-  
+
   return (
     <div className="cart">
+      <h2 style={{ textAlign: "center" }}>
+        {products.length > 0
+          ? `ยอดชำระรวม : ${formatMoney(total)} บาท`
+          : "ไม่มีสินค้าในตะกร้า"}
+      </h2>
       {products.map((data) => {
         return <Item key={data.id} {...data} />;
       })}
-      <h2 style={{textAlign:"center"}}>
-        {products.length > 0 ? `ยอดชำระรวม : ${formatMoney(total)} บาท` : "ไม่มีสินค้าในตะกร้า"}
-      </h2>
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,6 @@
 import "./Item.css";
 import { useCart } from "../context/CartContext";
+import { GoTrash } from "react-icons/go";
 
 export default function Item(props) {
   const { id, name, price, image, quantity } = props;
@@ -8,18 +9,36 @@ export default function Item(props) {
   return (
     <div className="item-container">
       <div className="card">
-        <img src={image} alt={id} />
-        <div className="product">
-          <p className="name">ชื่อสินค้า : {name}</p>
-          <p className="price">ราคา : {formatMoney(price)} บาท</p>
+      <img src={image} alt={id} />
+
+        <div className="card-grid">
+          <div className="product">
+            <p className="name">ชื่อสินค้า : {name}</p>
+            <p className="price">ราคา : {formatMoney(price)} บาท</p>
+          </div>
         </div>
-        <div className="quantity">
-          <button onClick={()=>addQuantity(id)}>+</button>
-          <input type="text" value={quantity} disabled />
-          <button onClick={()=>subTractQuantity(id)}>-</button>
+
+        <div className="card-grid">
+          <div className="quantity">
+            <button onClick={() => addQuantity(id)} className="increase">
+              +
+            </button>
+            <input type="text" value={quantity} disabled />
+            <button onClick={() => subTractQuantity(id)} className="decrease">
+              -
+            </button>
+          </div>
         </div>
-        <div className="total-price">{formatMoney(quantity * price)}</div>
-        <button onClick={()=>removeItem(id)}>Delete</button>
+
+        <div className="card-grid">
+          <div className="total-price">{formatMoney(quantity * price)} บาท</div>
+        </div>
+
+        <div className="card-grid">
+          <button onClick={() => removeItem(id)} className="delete">
+            <GoTrash />
+          </button>
+        </div>
       </div>
     </div>
   );
